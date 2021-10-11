b780516648fb6d72a3fcade5c4199bfa0fb37964
Richard Liu <richard.liu.tx@gmail.com>
2021-10-11 00:44:17 -0500

    update formatting for sliding window problems

--- Sliding Window/Sliding Window Fixed Size.py
+++ Sliding Window/Sliding Window Fixed Size.py
@@ -9,13 +9,13 @@ def sliding_window_fixed_size(arr, fixed_size):
 	for window_end in range(len(arr)):
 
 		# after we've done the preliminary expanding, at the beginning of each loop, 
-    	# our window will be of size k - 1
-
-    	# expand the window by 1 to make the window size k
+		# our window will be of size k - 1
+		
+		# expand the window by 1 to make the window size k
 		current_window += arr[window_end]
 
 		# only update max size and shrink once we've reached a window of size k
-    	# otherwise, we just initially grow our window to size k
+		# otherwise, we just initially grow our window to size k
 		if window_end >= fixed_size:
 
 			update_optimal_window(optimal_window, current_window)
