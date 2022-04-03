e5e4de30ccccea792869f923f1835f4b54205b97
Richard Liu <richard.liu.tx@gmail.com>
2022-04-03 11:58:47 -0500

    Substring with Concatenation naive solution

--- Sliding Window/Substring with Concatenation of All Words.py
+++ Sliding Window/Substring with Concatenation of All Words.py
@@ -1,61 +1,38 @@
-from typing import List
-from collections import Counter
-def findSubstring(self, s: str, words: List[str]) -> List[int]:
-    result = []
+class Solution:
+    
+    def substringMatch(self, substring, word_freqs, words_remaining, word_len):
+        for i in range(0, words_remaining * word_len, word_len):
 
-    if len(words) == 0:
-        return result
+            current_string = substring[i:i + word_len]
+            if current_string in word_freqs:
 
-    word_freqs = Counter(words)
+                if word_freqs[current_string] > 0:
 
-    current_remaining_words = len(words)
+                    word_freqs[current_string] -= 1
+                    words_remaining -= 1
+                else:
+                    return False
 
-    word_length = len(words[0])
-
-    left = 0
-
-    for right in range(0, len(s), word_length):
-        print("right: " + str(right))
-
-        current_word = s[right:right + 3]
-        print("current_word: " + current_word)
-
-        if current_word in word_freqs and word_freqs[current_word] > 0:
-
-            current_remaining_words.remove(current_word)
-            print(current_word + " is in the list, removing from remaining words")
-            print("remaining words: " + str(current_remaining_words))
-
-            if len(current_remaining_words) == 0:
-                print("No more remaining words, adding index " + str(left) + " to result")
-                # found a concatenation
-                result.append(left)
-
-        else:
-
-            print(current_word + " not in the list, adding all words back")
-
-
-            # add all words back
-            current_remaining_words.clear()
-            current_remaining_words = word_set.copy()
-
-            if current_word in current_remaining_words:
-                current_remaining_words.remove(current_word)
-                left = right
-                right -= 3
-
-                if len(current_remaining_words) == 0:
-                # found a concatenation
-                    result.append(left)
             else:
 
-                left = right + 3
+                return False
 
+        return words_remaining == 0
 
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        result = []
+        
+        if len(words) == 0:
+            return result
 
+        word_len = len(words[0])
+        
+        word_freqs = Counter(words)
+        
+        for i in range(len(s)):
 
-    return result
+            if self.substringMatch(s[i:i+word_len * len(words)], word_freqs.copy(), len(words), word_len):
 
-RESULT = findSubstring(None, "barfoofoobarthefoobarman", ["bar","foo","the"])
-print("RESULT: " + str(RESULT))
+                result.append(i)
+
+        return result
