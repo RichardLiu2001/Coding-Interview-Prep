56a81a104bc8c9c66b1b09cd6691458782236f62
Richard Liu <richard.liu.tx@gmail.com>
2022-04-02 19:11:08 -0500

    substring concat wip

--- Sliding Window/Substring with Concatenation of All Words.py
+++ Sliding Window/Substring with Concatenation of All Words.py
@@ -1,13 +1,14 @@
 from typing import List
+from collections import Counter
 def findSubstring(self, s: str, words: List[str]) -> List[int]:
     result = []
 
     if len(words) == 0:
         return result
 
-    word_set = set(words)
+    word_freqs = Counter(words)
 
-    current_remaining_words = set(words)
+    current_remaining_words = len(words)
 
     word_length = len(words[0])
 
@@ -19,7 +20,7 @@ def findSubstring(self, s: str, words: List[str]) -> List[int]:
         current_word = s[right:right + 3]
         print("current_word: " + current_word)
 
-        if current_word in current_remaining_words:
+        if current_word in word_freqs and word_freqs[current_word] > 0:
 
             current_remaining_words.remove(current_word)
             print(current_word + " is in the list, removing from remaining words")
