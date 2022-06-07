0888d8844d12d58a70948c322de5e23afa94e01f
Richard Liu <richard.liu.tx@gmail.com>
2022-06-06 23:11:13 -0700

    palindrome partitioning finish

--- Backtracking/Palindrome Partitioning.py
+++ Backtracking/Palindrome Partitioning.py
@@ -8,6 +8,7 @@ class Solution:
         if len(s) == 0:
             return result
 
+        # palindromeMemo[i][j] indicates if s[i:j] is a palindrome
         palindromeMemo = [[False for _ in range(len(s))] for _ in range(len(s))]
 
         self.partitionBacktrack(s, 0, [], result, palindromeMemo)
@@ -15,22 +16,32 @@ class Solution:
         return result
 
 
-    def partitionBacktrack(self, s, currentIndex, currentPartition, result, palindromeMemo):
+    def partitionBacktrack(self, s, beginPalindromeIndex, currentPartition, result, palindromeMemo):
 
-        if currentIndex == len(s):
+        if beginPalindromeIndex == len(s):
 
             result.append(currentPartition[:])
             return
         
-        currentChar = s[currentIndex]
+        currentChar = s[beginPalindromeIndex]
 
         # choice: where to end the current palindrome
-        for end in range(currentIndex, len(s)):
+        for end in range(beginPalindromeIndex, len(s)):
 
-            # if isPalindrome(s[currentIndex:end+1]):
-            if currentChar == s[end] and (currentIndex + 1 == end or currentIndex == len(s) - 1 or palindromeMemo[currentIndex + 1][end - 1]):
+            #if self.isPalindrome(s[currentIndex:end+1]):
 
+            # checking if the substring from currentIndex to end+1 is a palindrome
+            
+            # if the end char matches the beginning char (could be the same index) and either (the end char is <= 2 ahead aka 
+            # the substring is of length 1 or 2, or everything in the middle is already known to be a palindrome)
+            if s[end] == s[beginPalindromeIndex] and (end - beginPalindromeIndex <= 2 or palindromeMemo[beginPalindromeIndex + 1][end - 1]):
+
+                palindromeMemo[beginPalindromeIndex][end] = True
                 # make choice to end the current palindrome at index end (end + 1 for substring indexing)
-                currentPartition.append(s[currentIndex:end+1])
+                currentPartition.append(s[beginPalindromeIndex:end+1])
                 self.partitionBacktrack(s, end + 1, currentPartition, result, palindromeMemo)
                 currentPartition.pop()
+
+    def isPalindrome(self, a):
+
+        return a == a[::-1]
