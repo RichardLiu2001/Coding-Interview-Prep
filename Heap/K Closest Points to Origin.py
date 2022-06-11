63e7d95f24f652753bba34d64c62a2cb9b5747d0
Richard Liu <richard.liu.tx@gmail.com>
2022-06-11 11:22:15 -0700

    update k cloeset

--- Heap/K Closest Points to Origin.py
+++ Heap/K Closest Points to Origin.py
@@ -1,19 +1,37 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         
-        point_min_heap = [Point(p[0], p[1]) for p in points]
+        point_max_heap = [Point(p[0], p[1]) for p in points]
 
-        heapq.heapify(pointMinHeap)
+        heapq.heapify(point_max_heap)
 
         result = []
 
         for _ in range(k):
 
-            current_closest_point = heapq.heappop(point_min_heap)
+            current_closest_point = heapq.heappop(point_max_heap)
 
             result.append([current_closest_point.x, current_closest_point.y])
 
         return result
+
+    def kClosestLeetCode(points, k):
+
+        point_max_heap = []
+
+        for i in range(k):
+
+            current_point = points[i]
+            point_max_heap.append(Point(current_point[0], current_point[1]))
+
+
+        for i in range(k, len(points)):
+
+            current_point = points[i]
+
+
+
+
 
 class Point:
 
@@ -24,4 +42,8 @@ class Point:
 
     def __lt__(self, other_point):
 
-        return self.x ** 2 + self.y ** 2 < other_point.x ** 2 + other_point.y ** 2
+        return self.euclidean_distance() < other_point.euclidean_distance()
+
+    def euclidean_distance(self):
+
+        return self.x ** 2 + self.y ** 2
