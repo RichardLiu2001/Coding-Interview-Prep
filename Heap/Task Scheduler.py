6c6b23821c01a3601fd0d0a763929bdd89349119
Richard Liu <richard.liu.tx@gmail.com>
2022-06-12 10:42:50 -0700

    task scheduler notes

--- Heap/Task Scheduler.py
+++ Heap/Task Scheduler.py
@@ -11,11 +11,16 @@ class Solution:
         # max frequency
         f_max = frequencies.pop()
 
+
         # minimum idle time from the tasks with the greatest frequency
         idle_time = (f_max - 1) * n
         
         while len(frequencies) > 0 and idle_time > 0:
-            idle_time -= min(f_max - 1, frequencies.pop())
+
+        # A _ _ A _ _ A -> B
+        # We can occupy at most f_max - 1 (2) idle spots
+        idle_time -= min(f_max - 1, frequencies.pop())
+        
         idle_time = max(0, idle_time)
 
         return idle_time + len(tasks)
