679b2d77e694aae59285499b8eb117f92602e8b9
Richard Liu <richard.liu.tx@gmail.com>
2022-06-18 11:17:38 -0700

    tic tac toe better solution

--- OOP Design/Tic Tac Toe/Find Winner on a Tic Tac Toe Game.py
+++ OOP Design/Tic Tac Toe/Find Winner on a Tic Tac Toe Game.py
@@ -1,3 +1,44 @@
+class Solution:
+    def tictactoe(self, moves: List[List[int]]) -> str:
+        
+        # store the sum of the playerIds on each row and column
+        # rows[0] represents the sum of the playerIds on row 0
+        rowSums = [0, 0, 0]
+        colSums = [0, 0, 0]
+
+        # store the sum of the playerIds on each diag (only 1 diag and 1 antidiag, so don't need an array)
+        diagSum = 0
+        antiDiagSum = 0
+
+        currentPlayerId = 1
+
+        for move in moves:
+
+            moveRow, moveCol = move[0], move[1]
+
+            rowSums[moveRow] += currentPlayerId
+            colSums[moveCol] += currentPlayerId
+
+            if moveRow == moveCol:
+                diagSum += currentPlayerId
+
+            if moveRow + moveCol == 2:
+                antiDiagSum += currentPlayerId
+
+            if abs(rowSums[moveRow]) == 3 or abs(colSums[moveCol]) == 3 \
+                or abs(diagSum) == 3 or abs(antiDiagSum) == 3:
+
+                return 'A' if currentPlayerId == 1 else 'B' 
+
+            currentPlayerId *= -1
+
+
+        if len(moves) == 9:
+            return 'Draw'
+
+        return 'Pending'
+
+
 class BruteForceSolution:
     def tictactoe(self, moves: List[List[int]]) -> str:
         
