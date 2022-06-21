b1af56c7c19a7e85396638cde3c64598786b6fbd
Richard Liu <richard.liu.tx@gmail.com>
2022-06-20 20:17:28 -0700

    solution with quickselect

--- Heap/Kth Largest Element in an Array.py
+++ Heap/Kth Largest Element in an Array.py
@@ -2,20 +2,39 @@ class Solution:
 
     def findKthLargest(self, nums: List[int], k: int) -> int:
 
+        kth_smallest_index = len(nums) - k
 
-    def quickSelect(self, nums, index, l, r):
+        return self.quickSelect(kthLargestIndex, nums, 0, len(nums) - 1)
 
-        current_pointer = l 
+    def quickSelect(self, , nums, l, r):
+
+        pivot_separating_index = l
         # for simplicity, always choose rightmost value as the pivot
         # could randomly pick any value between l and r inclusive
         pivot = nums[r]
 
         # don't include r since it is the pivot
-        for i in range(l, r)
-            current_num = nums[l]
+        for i in range(l, r):
+            current_num = nums[i]
 
-            if current_num 
+            if current_num <= pivot:
 
+                nums[currentIndex], nums[i] = nums[i], nums[currentIndex]
+                currentIndex += 1
+
+
+        nums[currentIndex], nums[r] = nums[r], nums[currentIndex]
+
+        if currentIndex > k:
+
+            return self.quickSelect(k, nums, l, currentIndex - 1)
+
+        if currentIndex < k:
+
+            return self.quickSelect(k, nums, currentIndex + 1, r)
+
+        # p == k
+        return nums[currentIndex]
 
 
     def findKthLargestHeap(self, nums: List[int], k: int) -> int:
