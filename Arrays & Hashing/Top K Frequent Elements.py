ae8b2bd729466ba286093f431eded6dbd887f1d0
Richard Liu <richard.liu.tx@gmail.com>
2022-06-21 23:39:03 -0700

    Top K Frequent Elements

--- Arrays & Hashing/Top K Frequent Elements.py
+++ Arrays & Hashing/Top K Frequent Elements.py
@@ -1,5 +1,38 @@
 class Solution:
+
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+        # frequencies of each number
+        num_freqs = Counter(nums)
+
+        # list of buckets (lists) - index i holds the list of numbers that occur i times
+        freq_buckets = [[] for i in range(len(nums) + 1)]
+
+        for num in num_freqs:
+
+            num_freq = num_freqs[num]
+
+            freq_buckets[num_freq].append(num)
+
+        result = []
+        
+        # get elements in reverse order of buckets (from greatest to least frequency)
+        for i in range(len(freq_buckets) - 1, 0, -1):
+
+            current_freq_bucket = freq_buckets[i]
+
+            for num in current_freq_bucket:
+
+                result.append(num)
+
+                if len(result) == k:
+
+                    return result
+
+        return result
+        
+
+    def topKFrequentHeap(self, nums: List[int], k: int) -> List[int]:
         
         num_freqs = Counter(nums)
 
