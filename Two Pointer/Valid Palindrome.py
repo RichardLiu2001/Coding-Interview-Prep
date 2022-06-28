e9c256efbaebd2770c1a0d5c4298994d0ed2408a
Richard Liu <richard.liu.tx@gmail.com>
2022-06-27 23:52:46 -0700

    Valid Palindrome

--- Two Pointer/Valid Palindrome.py
+++ Two Pointer/Valid Palindrome.py
@@ -7,7 +7,16 @@ class Solution:
 
         while left < right:
 
-            if s[left] != s[right]:
+            # skip left pointer until next alphanumeric
+            # need left < right condition since it could be broken when incrementing left
+            while left < right and not s[left].isalnum():
+                left += 1
+
+            # skip right pointer until first alphanumeric
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            if s[left].lower() != s[right].lower():
                 return False
 
             left += 1
