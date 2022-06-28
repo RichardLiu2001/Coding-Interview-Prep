4ee3cbe15e3fb847fa91c321a42a2402b2b1bb9f
Richard Liu <richard.liu.tx@gmail.com>
2022-06-28 01:06:07 -0700

    3sum updates

--- Two Pointer/3Sum.py
+++ Two Pointer/3Sum.py
@@ -8,23 +8,27 @@ class Solution:
 
             sum = nums[i] + nums[left] + nums[right]
 
-            if sum == 0:
-                result.append([nums[i], nums[left], nums[right]])
-                left += 1
+            if sum > 0:
+
                 right -= 1
+            
+            elif sum < 0:
+
+                left += 1
+            
+            else:
+                # sum == 0
+                result.append(nums[i], nums[left], nums[right])
+                left += 1
+                right -=1 
 
                 # skip duplicates starting from the left, to get unique triplets
                 while left < right and nums[left] == nums[left - 1]:
                     left += 1
-                #while left < right and nums[right] == nums[right + 1]:
-                #    right -= 1
-                
-            elif sum < 0:
-                left += 1
 
-            else:
-                right -= 1
-
+                # once we have a different left, even if the right pointer encounters
+                # a duplicate value from before, it will no longer create a valid triplet
+                # since the left pointer now points to a different number.
 
 
     def threeSum(self, nums: List[int]) -> List[List[int]]:
@@ -37,6 +41,11 @@ class Solution:
         result = []
 
         for i in range(len(nums)):
+
+            # optimization: since nums is sorted, anything greater than 0
+            # cannot have pairs later on that will make the sum 0.
+            if nums[i] > 0:
+                break
 
             # we want to skip duplicate numbers, which will be next to 
             # each other because the array is sorted
