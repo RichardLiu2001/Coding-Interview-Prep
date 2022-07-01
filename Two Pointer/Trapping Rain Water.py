bdf5b4b524e4985a090f4e5cd892ada91f683d88
Richard Liu <richard.liu.tx@gmail.com>
2022-07-01 00:17:39 -0700

    trapping rain water

--- Two Pointer/Trapping Rain Water.py
+++ Two Pointer/Trapping Rain Water.py
@@ -1,11 +1,61 @@
 class Solution:
+    # two pointer O(N) time O(1) space solution
     def trap(self, height: List[int]) -> int:
+
+        left = 0
+        right = len(height) - 1
+        left_max = 0
+        right_max = 0
+
+        total_trapped_water = 0
+        
+        # must be <= since we process and then move the index
+        while left <= right:
+            
+            # we choose which side to process from based on whichever max is 
+            # smaller, since that side will be the bottleneck. In addition,
+            # since we started the left and right pointers at the beginning and 
+            # end of the array respectively, the corresponding direction max height
+            # (left_max for left and right_max for right) is guaranteed to be correct.
+            if left_max < right_max:
+
+                current_height = height[left]
+                
+                # although we don't know the true height of the right_max wall,
+                # it doesn't matter since the left_max wall is already shorter
+                # than what we currently have for right_max
+
+                # the current trapped water is bottlenecked by the left_max wall,
+                # which is the shorter of the two max walls
+                current_trapped_water = left_max - current_height
+
+                # If the current wall is taller than both right and left max,
+                # no water can be trapped.
+                if current_trapped_water > 0:
+                    total_trapped_water += current_trapped_water
+                
+                left_max = max(left_max, current_height)
+                left += 1
+
+            else:
+
+                current_height = height[right]
+
+                current_trapped_water = right_max - current_height
+    
+                if current_trapped_water > 0:
+                    total_trapped_water += current_trapped_water
+                
+                right_max = max(right_max, current_height)
+                right -= 1
+
+        return total_trapped_water
+
+    # DP O(N) time O(N) space solution
+    def trapDP(self, height: List[int]) -> int:
 
         highest_to_left = [0 for _ in range(len(height))]
         highest_to_right = [0 for _ in range(len(height))]
-
-        highest_to_left[0] = height[0]
-        highest_to_right[-1] = height[-1]
 
         # populate highest_to_left
         for i in range(1, len(height)):
@@ -30,7 +80,7 @@ class Solution:
 
             left_highest_height = highest_to_left[i]
             right_highest_height = highest_to_right[i]
-            
+
             if left_highest_height > current_wall_height and right_highest_height > current_wall_height:
 
                 current_index_trapped_water = min(left_highest_height, right_highest_height) - current_wall_height
