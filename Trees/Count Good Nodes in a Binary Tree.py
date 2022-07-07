99e6d2b0bbfd39648afa5e5d72c43cfe17621c85
Richard Liu <richard.liu.tx@gmail.com>
2022-07-06 23:26:16 -0700

    Count Good Nodes in a Binary Tree solution

--- Trees/Count Good Nodes in a Binary Tree.py
+++ Trees/Count Good Nodes in a Binary Tree.py
@@ -7,3 +7,24 @@
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
         
+        # Question just asks for how many, can use a static int or list of length 1
+        good_nodes = []
+
+        self.goodNodesDFSHelp(root, float("-inf"), good_nodes)
+
+        return len(good_nodes)
+
+
+    def goodNodesDFSHelp(self, node, max_so_far, good_nodes):
+
+        if not node:
+            return
+
+        if node.val >= max_so_far:
+
+            good_nodes.append(node.val)
+
+            max_so_far = node.val
+
+        self.goodNodesDFSHelp(node.left, max_so_far, good_nodes)
+        self.goodNodesDFSHelp(node.right, max_so_far, good_nodes)
