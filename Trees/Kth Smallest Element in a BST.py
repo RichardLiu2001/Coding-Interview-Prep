effe309f95ae9241747c086c0b22f644edb99087
Richard Liu <richard.liu.tx@gmail.com>
2022-07-11 22:50:54 -0700

    kth smallest in a bst wip

--- Trees/Kth Smallest Element in a BST.py
+++ Trees/Kth Smallest Element in a BST.py
@@ -7,3 +7,35 @@
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         
+        # sorted_nums = []
+        # self.kthSmallestHelp(node, sorted_nums)
+        # return sorted_nums[k - 1]
+        result = [0, -1]
+        self.kthSmallestIndex(root, k, result)
+        return result[1]
+
+    def kthSmallestHelp(self, node, sorted_nums):
+
+        if not node:
+            return
+
+        self.kthSmallestHelp(node.left, sorted_nums)
+        sorted_nums.append(node.val)
+        self.kthSmallestHelp(node.right, sorted_nums)
+
+
+    def kthSmallestIndex(self, node, k, order_num):
+
+        if not node or order_num[0] == k:
+            return
+
+        self.kthSmallestIndex(node.left, k, order_num)
+
+        order_num[0] += 1
+        if order_num[0] == k:
+            order_num[1] = node.val
+
+        print(str(order_num[0]) + " smallest is " + str(node.val))
+        return
+
+        self.kthSmallestIndex(node.right, k, order_num)
