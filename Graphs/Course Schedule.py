db7686c37f9736134ee6d26208d9d8435fa97c58
Richard Liu <richard.liu.tx@gmail.com>
2022-07-21 22:38:58 -0700

    finish course schedule

--- Graphs/Course Schedule.py
+++ Graphs/Course Schedule.py
@@ -10,12 +10,16 @@ class Solution:
 
             next_courses[prerequisite].append(next_course)
 
+        # Stores courses whose next courses can be reached with no prereq conflicts.
+        # This prevents rechecking nodes - if node B is valid, and A is a prereq of B, 
+        # when we check A, we don't need to check B again.
+        verified = set()
 
         for course in range(numCourses):
 
             # We have to run DFS from every course because there could be 
             # unconnected components.
-            if self.contains_cycle(course, set(), next_courses):
+            if self.contains_cycle(course, set(), verified, next_courses):
                 return False
 
         return True
@@ -24,7 +28,12 @@ class Solution:
     # was found. 
     # Corresponds to answering the question, "If I start with the current_course, 
     # will any next courses have impossible prereq conflicts?"
-    def contains_cycle(self, current_course, current_path, next_courses):
+    # We use a set to "breadcrumb" our current_path - we keep track of the current
+    # path that we're on, and if we reach a node that's on it, we know that there is a cycle.
+    def contains_cycle(self, current_course, current_path, verified, next_courses):
+
+        if current_course in verified:
+            return False
 
         if current_course in current_path:
             return True
@@ -33,10 +42,22 @@ class Solution:
 
         for next_course in next_courses[current_course]:
 
-            if self.contains_cycle(next_course, current_path, next_courses):
+            if self.contains_cycle(next_course, current_path, verified, next_courses):
                 return True
 
+        # Remove the current node from the current path because it is no longer on 
+        # the current path when we go check other paths.
+        # NOTE: this is a special case where we don't actually need to do this. The 
+        # current_path acts as our visited set in DFS, and if we encounter an already visited 
+        # node, we know that there is a cycle and break out of the DFS immediately. Thus,
+        # it doesn't matter if we don't remove it from the current path - the DFS is going to end
+        # and return True (there is a cycle).
+
+        # In the normal case of DFS, we don't break out of the entire DFS - 
+        # we just use the visited set to know not to visit those nodes again.
         current_path.remove(current_course)
 
-        return False
+        # Mark this node as verified, so we don't recheck it in future DFS's.
+        verified.add(current_course)
 
+        return False
