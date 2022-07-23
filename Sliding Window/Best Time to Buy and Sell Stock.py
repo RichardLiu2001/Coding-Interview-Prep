da2af5cfc4027ebd97758ed461d7567e6ab6b1cd
Richard Liu <richard.liu.tx@gmail.com>
2022-07-23 16:04:07 -0700

    Best Time to Buy and Sell Stock finished

--- Sliding Window/Best Time to Buy and Sell Stock.py
+++ Sliding Window/Best Time to Buy and Sell Stock.py
@@ -1,3 +1,20 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
+        max_profit = 0
+
+        # Lowest price we've found so far
+        lowest_so_far = float('inf')
+
+        for price in prices:
+
+            lowest_so_far = min(lowest_so_far, price)
+
+            # The most profit we can get with this current price is the difference between the current price 
+            # and the lowest price we've found so far before it.
+            current_max_profit = price - lowest_so_far
+
+            # Update overall max profit if the current profit is greater than any profit we found so far.
+            max_profit = max(max_profit, current_max_profit)
+
+        return max_profit
