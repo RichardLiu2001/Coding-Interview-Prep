d1addd6f89890e6d210acaae408e06d19117efd9
Richard Liu <richard.liu.tx@gmail.com>
2022-07-23 14:57:20 -0700

    Finish Topological Sort Algo

--- Algorithms/Topological Sort.py
+++ Algorithms/Topological Sort.py
@@ -3,22 +3,30 @@ from Graph import Graph
 
 def topological_sort(g: Graph):
 
+	# Stores the nodes that we have verified have no cycles from them if you run DFS starting at 
+	# that node.
 	verified = set()
 
+	# An ordering of the nodes in the graph such that for every edge u→v, u comes before v in the ordering.
 	topological_ordering = []
 
-	for node in range(g.num_vertices):
+	for node in list(g.graph):
 
 		if contains_cycle_dfs(node, set(), verified, topological_ordering, g):
 			return -1
 
+	# Reverse the order, since DFS added the nodes to the array from deepest to shallowest.
 	return topological_ordering[::-1]
 
 
+# Runs DFS from a node and checks if there is a cycle. 
 def contains_cycle_dfs(node, current_path, verified, topological_ordering, g):
+
+	# If the node is in the current path that we're searching, we've found a cycle.
 	if node in current_path:
 		return True
 
+	# If we've already checked this node has no cycles in another DFS, we don't need to check again.
 	if node in verified:
 		return False
 
@@ -28,7 +36,6 @@ def contains_cycle_dfs(node, current_path, verified, topological_ordering, g):
 	for neighbor in g.graph[node]:
 		if contains_cycle_dfs(neighbor, current_path, verified, topological_ordering, g):
 			return True
-
 
 	verified.add(node)
 
