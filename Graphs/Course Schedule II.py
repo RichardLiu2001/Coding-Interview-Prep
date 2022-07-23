183bdb09e34fbe4eac3f5d728b28a571003ebf95
Richard Liu <richard.liu.tx@gmail.com>
2022-07-22 23:57:42 -0700

    course schedule ii wip

--- Graphs/Course Schedule II.py
+++ Graphs/Course Schedule II.py
@@ -1,3 +1,15 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         
+        # Maps each course to the courses you can take after it (the 
+        # courses that have it as a prereq)
+        # This is how we represent the course prereq relationships as a graph (adjacency list)
+        next_courses = defaultdict(list)
+
+        for course, prereq in prerequisites:
+            next_courses[prereq].append(course)
+
+        
+
+        # Topological ordering (which order you need to take) of the courses 
+        course_ordering = []
