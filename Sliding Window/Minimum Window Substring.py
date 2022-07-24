48f39e26a9690c075560f393f55380f28aeb86d9
Richard Liu <richard.liu.tx@gmail.com>
2022-07-24 16:41:26 -0700

    update comments and defaultdict

--- Sliding Window/Minimum Window Substring.py
+++ Sliding Window/Minimum Window Substring.py
@@ -6,64 +6,55 @@ class Solution:
             return ""
 
         # frequencies of all characters in t
-        t_freqs = {}
+        t_freqs = defaultdict(int)
 
         # populate t_freqs
         for character in t:
 
-            if character not in t_freqs:
-                t_freqs[character] = 0
-
             t_freqs[character] += 1
-
 
         # how many unique characters we have left to find
         chars_remaining = len(t_freqs)
-
-        # how many total characters we have to find
-        #chars_remaining = len(t)
-
 
         min_window_left = 0
         min_window_right = len(s) - 1
         min_window_len = float("inf")
 
-        left = 0
+        window_start = 0
 
-        for right in range(len(s)):
+        for window_end in range(len(s)):
 
-            cur_letter = s[right]
+            cur_letter = s[window_end]
 
             if cur_letter in t_freqs:
                 t_freqs[cur_letter] -= 1
-
-                #if t_freqs[cur_letter] >= 0:
 
                 # reached quota for that one character
                 if t_freqs[cur_letter] == 0:
                     chars_remaining -= 1
 
-
+            # Keep shrinking until the window doesn't contain the substring. We may be able to get
+            # a more optimal solution than the initial window found if there are multiple excess
+            # characters in the beginning (etc. looking for ABC in AAAAABC) -> get rid of all but 1 of the beginning A's.
             while chars_remaining == 0:
 
-                if right - left + 1 < min_window_len:
-                    min_window_left = left
-                    min_window_right = right
-                    min_window_len = right - left + 1
+                current_window_length = window_end - window_start + 1
+
+                if current_window_length < min_window_len:
+                    min_window_left = window_start
+                    min_window_right = window_end
+                    min_window_len = current_window_length
 
                 # shrink window
-
-                left_char = s[left]
+                left_char = s[window_start]
 
                 if left_char in t_freqs:
                     t_freqs[left_char] += 1
-
-                    #if t_freqs[left_char] >= 1:
 
                     # now have 1 unique character to match, current window no longer contains all of t
                     if t_freqs[left_char] == 1:
                         chars_remaining += 1
 
-                left += 1
+                window_start += 1
 
         return "" if min_window_len == float("inf") else s[min_window_left:min_window_right + 1]
