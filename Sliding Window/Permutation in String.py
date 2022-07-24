912841c1e9f4836bf3f4da8a0854a1a731d17801
Richard Liu <richard.liu.tx@gmail.com>
2022-07-24 16:17:34 -0700

    add some comments

--- Sliding Window/Permutation in String.py
+++ Sliding Window/Permutation in String.py
@@ -5,16 +5,14 @@ class Solution:
             return False
 
         # stores the frequencies of all the characters in s1
-        s1_freqs = {}
+        s1_freqs = defaultdict(int)
 
         for character in s1:
-            if character not in s1_freqs:
-                s1_freqs[character] = 0
 
             s1_freqs[character] += 1
 
         # stores the frequencies of all the characters in the current window
-        window_freqs = {}
+        window_freqs = defaultdict(int)
 
         window_start = 0
 
@@ -22,16 +20,15 @@ class Solution:
         
             current_char = s2[window_end]
 
-            if current_char not in window_freqs:
-                window_freqs[current_char] = 0
-
             window_freqs[current_char] += 1
 
             # if the frequencies of the window and s1 match, we have an anagram
+            # == check is O(26)
             if window_freqs == s1_freqs:
                 return True
 
             # Only shrink (slide) the window if it is the length of s1
+            # We shrink once to mainain the size of the window, since the substring is a set size.
             if window_end - window_start + 1 == len(s1):
 
                 front_char = s2[window_start]
@@ -48,12 +45,11 @@ class Solution:
 
     def checkInclusion(self, s1: str, s2: str) -> bool:
 
-
         if len(s1) > len(s2):
             return False
 
         # stores the frequency of each character in s1
-        s1_freqs = {}
+        s1_freqs = defaultdict(int)
 
         # stores the number of characters in the current sliding window
         # that are all completely matched with the same character in s1
@@ -63,8 +59,6 @@ class Solution:
         all_matched_chars = 0
     
         for character in s1:
-            if character not in s1_freqs:
-                s1_freqs[character] = 0
 
             s1_freqs[character] += 1
         
@@ -74,6 +68,10 @@ class Solution:
 
             current_char = s2[window_end]
 
+            # If the current character is not in s1, we won't update s1_freqs or all_matched_chars.
+            # Furthermore, when we shrink the window and remove a character that's not in s1, 
+            # neither of those will be updated either.
+            # Thus, characters not in s1 don't affect the matching conditions at all.
             if current_char in s1_freqs:
 
                 s1_freqs[current_char] -= 1
@@ -90,8 +88,8 @@ class Solution:
                 return True
 
             # Only shrink (slide) the window if it is the length of s1
+            # We shrink once to maintain the size of the window, since the substring is a set size.
             if window_end - window_start + 1 == len(s1):
-
 
                 front_char = s2[window_start]
 
